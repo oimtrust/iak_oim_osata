015c403de77d8cded0309ffe0527531f37826a58
oimtrust <trustoim@gmail.com>
2016-05-03 16:50:19 +0700

    Update For Maps

--- app/src/main/java/com/oimtrust/osata/fragment/FragmentMaps.java
+++ app/src/main/java/com/oimtrust/osata/fragment/FragmentMaps.java
@@ -7,12 +7,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.oimtrust.osata.R;
-public class FragmentMaps extends Fragment {
+
+public class FragmentMaps extends Fragment implements OnMapReadyCallback{
+
+    private GoogleMap mapView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_fragment_maps, container, false);
+        View rootMaps = inflater.inflate(R.layout.activity_fragment_maps, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync( this);
+
+        return rootMaps;
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapView = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng malang = new LatLng(-7.9784695, 112.561742);
+        mapView.addMarker(new MarkerOptions().position(malang).title("Osata in Malang"));
+        mapView.moveCamera(CameraUpdateFactory.newLatLng(malang));
+    }
+
 }
